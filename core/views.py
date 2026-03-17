5e9a5dd91e27ee80ee0f6d484122c4ed04621606
Traore cheik idriss <ja391961@ens.uqam.ca>
2026-03-17 16:50:24 -0400

    Déplace section avis clients vers la page rendez-vous

--- core/views.py
+++ core/views.py
@@ -1,13 +1,4 @@
 from django.shortcuts import render
-from reviews.models import Review
-from reviews.forms import ReviewForm
 
 def home(request):
-    reviews = Review.objects.filter(approved=True)[:6]
-    form = ReviewForm()
-    submitted = request.GET.get('avis') == 'merci'
-    return render(request, "core/home.html", {
-        'reviews': reviews,
-        'form': form,
-        'submitted': submitted,
-    })
+    return render(request, "core/home.html")
